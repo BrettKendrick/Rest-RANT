9e7c8aabb0c9079fe9ac11f076ae00c92e342a98
BrettKendrick <123604258+BrettKendrick@users.noreply.github.com>
2023-08-31 19:10:04 -0700

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -4,18 +4,19 @@ const places = require('../Models/places.js')
 router.get('/:id', (req, res) => {
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id/edit', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
-    res.render('error404')
+      res.render('error404')
   }
   else if (!places[id]) {
-    res.render('error404')
+      res.render('error404')
   }
   else {
-    res.render('places/show', { place: places[id], id })
+    res.render('places/edit', { place: places[id] })
   }
 })
+
 
 
 
